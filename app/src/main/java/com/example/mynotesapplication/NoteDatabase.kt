5adafecfea29adb9d67f25019d27d38afeecbefd
rushanc <rushancubeywarna@gmail.com>
2024-05-15 02:44:14 +0530

    making the crud operations

--- app/src/main/java/com/example/mynotesapplication/NoteDatabase.kt
+++ app/src/main/java/com/example/mynotesapplication/NoteDatabase.kt
@@ -37,4 +37,24 @@ class NoteDatabase(context :Context) : SQLiteOpenHelper(context, DATABASE_NAME, 
         db.insert(TABLE_NAME, null, values)
         db.close()
     }
+
+    fun getAllNotes(): List<Note>{
+        val noteList = mutableListOf<Note>()
+        val db = readableDatabase
+        val query = "SELECT * FROM $TABLE_NAME"
+        val cursor = db.rawQuery(query, null)
+
+        while (cursor.moveToNext()){
+            val id = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_ID))
+            val title = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_TITLE))
+            val content = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_CONTENT))
+
+            val note = Note(id, title, content)
+            noteList.add(note)
+        }
+        cursor.close()
+        db.close()
+        return noteList
+
+    }
 }
