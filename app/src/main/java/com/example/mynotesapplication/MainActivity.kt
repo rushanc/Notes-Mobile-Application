382aa6311705a9c65480f8d87b651b152400c0cd
rushanc <rushancubeywarna@gmail.com>
2024-05-16 21:01:58 +0530

    updated

--- app/src/main/java/com/example/mynotesapplication/MainActivity.kt
+++ app/src/main/java/com/example/mynotesapplication/MainActivity.kt
@@ -6,19 +6,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mynotesapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var db: NoteDatabase
+    private lateinit var noteAdapter: NoteAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        db = NoteDatabase(this)
+        noteAdapter = NoteAdapter(db.getAllNotes(),this)
+
+        binding.RecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.RecyclerView.adapter = noteAdapter
+
         binding.AddButton.setOnClickListener{
             val intent = Intent(this, AddNotesActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        noteAdapter.refreshData(db.getAllNotes())
     }
 }
