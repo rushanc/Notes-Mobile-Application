2fbfb1fd53740b0fca7b0cd5f1b623ba4f0f0a46
rushanc <rushancubeywarna@gmail.com>
2024-05-17 13:18:55 +0530

    Implement the delete operation

--- app/src/main/java/com/example/mynotesapplication/NoteDatabase.kt
+++ app/src/main/java/com/example/mynotesapplication/NoteDatabase.kt
@@ -84,4 +84,12 @@ class NoteDatabase(context :Context) : SQLiteOpenHelper(context, DATABASE_NAME, 
         db.close()
         return Note(id, title, content)
     }
+
+    fun deleteNote(noteId:Int){
+        val db = writableDatabase
+        val whereClause = "$COLUMN_ID = ?"
+        val whereArgs = arrayOf(noteId.toString())
+        db.delete(TABLE_NAME,whereClause,whereArgs)
+        db.close()
+    }
 }
